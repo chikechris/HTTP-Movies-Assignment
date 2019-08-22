216e0c0d9c40b5fbfef35823d2befe204356e00f
chikechris <cchike2017@gmail.com>
2019-08-22 15:39:53 -0400

    id numberis beign recieved back in movieForm

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -1,48 +1,48 @@
-import React from "react";
-import axios from "axios";
-import MovieCard from "./MovieCard";
+import React from 'react'
+import axios from 'axios'
+import MovieCard from './MovieCard'
+import { Link } from 'react-router-dom'
+
 export default class Movie extends React.Component {
-  constructor(props) {
-    super(props);
+  constructor (props) {
+    super(props)
     this.state = {
       movie: null
-    };
-  }
-
-  componentDidMount() {
-    this.fetchMovie(this.props.match.params.id);
-  }
-
-  componentWillReceiveProps(newProps) {
-    if (this.props.match.params.id !== newProps.match.params.id) {
-      this.fetchMovie(newProps.match.params.id);
     }
   }
-
+  componentDidMount () {
+    this.fetchMovie(this.props.match.params.id)
+  }
+  componentWillReceiveProps (newProps) {
+    if (this.props.match.params.id !== newProps.match.params.id) {
+      this.fetchMovie(newProps.match.params.id)
+    }
+  }
   fetchMovie = id => {
     axios
       .get(`http://localhost:5000/api/movies/${id}`)
-      .then(res => this.setState({ movie: res.data }))
-      .catch(err => console.log(err.response));
-  };
-
+      .then(res => {
+        console.log(res.data)
+        this.setState({ movie: res.data })
+      })
+      .catch(err => console.log(err.response))
+  }
   saveMovie = () => {
-    const addToSavedList = this.props.addToSavedList;
-    addToSavedList(this.state.movie);
-  };
-
-  render() {
+    const addToSavedList = this.props.addToSavedList
+    addToSavedList(this.state.movie)
+  }
+  render () {
     if (!this.state.movie) {
-      return <div>Loading movie information...</div>;
+      return <div>Loading movie information...</div>
     }
-
     return (
-      <div className="save-wrapper">
+      <div className='save-wrapper'>
         <MovieCard movie={this.state.movie} />
-        <div className="save-button" onClick={this.saveMovie}>
+        <div className='save-button' onClick={this.saveMovie}>
           Save
         </div>
+        <Link to={`/add-movie/${this.state.movie.id}`}>Update Movie</Link>
       </div>
-    );
+    )
   }
 }
