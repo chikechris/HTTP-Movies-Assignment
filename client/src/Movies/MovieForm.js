aeb153145bd4005bb39e003a99ae80efdee6cef8
chikechris <cchike2017@gmail.com>
2019-08-22 16:08:00 -0400

    WIP

--- client/src/Movies/MovieForm.js
+++ client/src/Movies/MovieForm.js
@@ -1,29 +1,31 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-function MovieForm(props) {
-  console.log(props.match.params.id);
-
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+function MovieForm (props) {
+  console.log(props.match.params.id)
   const [updateMovie, setUpdateMovie] = useState({})
-
   useEffect(() => {
-    axios.get(`http://localhost:5000/api/movies/${props.match.params.id}`)
+    axios
+      .get(`http://localhost:5000/api/movies/${props.match.params.id}`)
       .then(res => {
         console.log(res.data)
         setUpdateMovie({ ...res.data })
       })
       .catch(err => console.log(err))
   }, [])
-
-  const updateSubmitMovie = (event) => {
-    event.preventDefault();
+  const updateSubmitMovie = event => {
+    event.preventDefault()
     console.log(updateMovie)
+    axios
+      .put(
+        `http://localhost:5000/api/movies/${props.match.params.id}`,
+        updateMovie
+      )
+      .then(res => console.log(res))
+      .catch(err => console.log(err))
   }
-
   const handleChange = event => {
     setUpdateMovie({ ...updateMovie, [event.target.name]: event.target.value })
   }
-
-
   return (
     <div className='movieform-container'>
       <form onSubmit={updateSubmitMovie}>
@@ -36,6 +38,7 @@ function MovieForm(props) {
         <input
           type='text'
           name='metascore'
+          value={Number(updateMovie.metascore)}
           value={updateMovie.metascore}
           onChange={handleChange}
         />
@@ -47,8 +50,7 @@ function MovieForm(props) {
         />
         <button>Submit Changes</button>
       </form>
-
     </div>
   )
 }
-export default MovieForm;
+export default MovieForm
